a7731cd1b992837a9ff5bc1c21c5ef7fc40df0db
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-02-08 18:02:27 +0100

    Form_Validation

--- src/app/components/SignUpForm.tsx
+++ src/app/components/SignUpForm.tsx
@@ -11,10 +11,53 @@ import {
 import { Button, Checkbox, Input } from "@nextui-org/react";
 import Link from 'next/link';
 import { useState } from "react";
+import { z, refine  } from "zod";
+import validator from 'validator';
+
+const FormSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, "First name must be atleast 2 characters")
+    .max(45, "First name must be less than 45 characters")
+    .regex(new RegExp("^[a-zA-Z]+$"), "No specail character allowed!"),
+  lastName: z
+    .string()
+    .min(2, "Last name must be atleast 2 characters")
+    .max(45, "Last name must be less than 45 characters")
+    .regex(new RegExp("^[a-zA-Z]+$"), "No specail character allowed!"),
+  email: z
+    .string().email("Please enter a valid email address"),
+  phone: z
+    .string().refine(
+      value => validator.isMobilePhone(value, 'ar-MA'),
+      {
+        message: 'Veuillez saisir un numéro de téléphone valide pour le Maroc!'
+      }),
+  password: z
+    .string().min(6, "Password must be at least 6 characters")
+    .max(6, "Password must be at less 6 characters"),
+  confirmPassword: z
+    .string().min(6, "confirm Password must be at least 6 characters")
+    .max(6, "confirm Password must be at less 6 characters"),
+  accepted: z.
+    literal(true, {
+      errorMap: () => ({
+        message: "Please accept all terms",
+    })
+  })
+})
+.refine((data) => data.password === data.confirmPassword, {
+  message: "Password and confirm password doesn't match",
+  // afficher l'erreur sur password et confirm password
+  path: ["password", "confirmPassword"]
+});
+
 
 const SignUpForm = () => {
   const [isVisiblePass, setIsVisiblePass] = useState(false);
+  const [isVisibleConfirmPass, setIsVisibleConfirmPass] = useState(false);
   const toggleVisiblePass = () => setIsVisiblePass(prev => !prev);
+  const toggleVisibleConfirmPass = () => setIsVisibleConfirmPass(prev => !prev);
   return (
     <form className="grid grid-cols-2 gap-3 p-2 place-self-stretch shadow border rounded-md">
       <Input label="First Name" startContent={<UserIcon className="w-4" />} />
@@ -41,9 +84,11 @@ const SignUpForm = () => {
       <Input
         className="col-span-2"
         label="Confirm Password"
-        type={isVisiblePass ? "text" : "password"}
-        type={!isVisiblePass ? "text" : "password"}
+        type={isVisibleConfirmPass ? "text" : "password"}
         startContent={<KeyIcon className="w-4" />}
+        endContent={!isVisibleConfirmPass ? <EyeIcon className="w-4 cursor-pointer" onClick={toggleVisibleConfirmPass} />
+        : 
+        (<EyeSlashIcon className="w-4 cursor-pointer" onClick={toggleVisibleConfirmPass} />)}
       />
       <Checkbox className="col-span-2">I Accept The <Link href="/terms" className="text-sky-600">Terms</Link></Checkbox>
       <div className="flex justify-center col-span-2">
