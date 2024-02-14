1eb4623602a060658c27abbb8b474c6bb69e671b
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-02-14 12:40:17 +0100

    Setup_Third_Party_SMTP_Server_(Mailtrap)

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -9,32 +9,45 @@ export async function sendMail({
   subject: string;
   body: string;
 }) {
-  const { SMTP_EMAIL, SMTP_GMAIL_PASS } = process.env;
-  
-  const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: SMTP_EMAIL,
-      pass: SMTP_GMAIL_PASS,
+   
+   /*
+   const { SMTP_EMAIL, SMTP_GMAIL_PASS } = process.env;
+   const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+         user: SMTP_EMAIL,
+         pass: SMTP_GMAIL_PASS,
     },
   });
-   // ce code pour verifie le transport
+  */
+  const { SMTP_USER, SMTP_PASS } = process.env;
+
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: SMTP_USER,
+      pass: SMTP_PASS,
+    },
+  });
+  // ce code pour verifie le transport
+  // si tout va bien il renvoie <Test Result Of Transport true>
   try {
-   const testResult = await transport.verify();
-   console.log("Test Result Of Transport", testResult)
-  } catch(e) {
-   console.log("error ", e)
+    const testResult = await transport.verify();
+    console.log("Test Result Of Transport", testResult);
+  } catch (e) {
+    console.log("error ", e);
   }
 
   try {
-   const sendResult = await transport.sendMail({
+    const sendResult = await transport.sendMail({
       from: SMTP_EMAIL,
       to,
       subject,
-      html: body
-   });
-   console.log(sendResult);
+      html: body,
+    });
+    console.log(sendResult);
   } catch (error) {
-   console.log(error);
+    console.log(error);
   }
 }
